409817b6905423bd3adb69c8011ae090b71c8fce
margretheby <margretheby@users.noreply.github.com>
2023-05-24 10:46:13 +0200

    Added BidBox to projects

---
+++ js/script.js
@@ -0,0 +1,9 @@
+import { hamburgerMenu, hamburgerMenuTransform } from "./variables.js";
+/*
+function checkedBox() {
+    hamburgerMenuTransform.addEventListener("click", (event) => {
+        hamburgerMenuTransform.innerHTML = `Hello`;
+    })
+}
+
+checkedBox(); */
---
+++ js/variables.js
@@ -0,0 +1,2 @@
+export const hamburgerMenu = document.querySelector("#checkbox");
+export const hamburgerMenuTransform = document.querySelector(".checkbox");
